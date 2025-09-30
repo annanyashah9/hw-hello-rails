70fd287fd13d47df7e229a3c4e68560698b7b2e2
Ananya Shah <acs9931@nyu.edu>
2025-09-30 18:57:51 +0000

    Part 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  root "hello#index"           # homepage → hello#index
-
-  # (optional) if you also want /hello to work directly:
-  # get "hello", to: "hello#index"
-
-  # You can leave the health check route if you want:
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :movies
+  root :to => redirect('/movies')
 end
